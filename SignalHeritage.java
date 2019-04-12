517d61b21302e6350ccbc3b96af3ec31aa9d900f
Nicolas HERAND <nherand@enssat.fr>
2019-04-12 09:17:20 +0200

    ON ENLEVE LES ACCENTS MONSIEUR

--- SignalHeritage.java
+++ SignalHeritage.java
@@ -2,16 +2,16 @@ package RMI_Main;
 
 import java.rmi.server.UnicastRemoteObject;
 
-public class SignalHéritage extends UnicastRemoteObject implements monSignal{
+public class SignalHeritage extends UnicastRemoteObject implements monSignal{
     private int id = -1;
     private Object message = null;
 
-    public SignalHéritage() throws java.rmi.RemoteException{
+    public SignalHeritage() throws java.rmi.RemoteException{
         super();
         this.id = 1;
         this.message = "init";
     }
-    public SignalHéritage(int a,Object msg) throws java.rmi.RemoteException{
+    public SignalHeritage(int a, Object msg) throws java.rmi.RemoteException{
         this.id = a;
         this.message = msg;
     }
